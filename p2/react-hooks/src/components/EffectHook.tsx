85b08c74f7414bf6c5c91d1234b9c0adf39bc6e2
Esau Martinez <esaumtz4722@gmail.com>
2022-10-24 19:21:39 -0500

    feat: add effect hook

---
+++ p2/react-hooks/src/components/EffectHook.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface EffectHookInterface { }
+
+const EffectHook = ({ }: EffectHookInterface) => {
+    return <div>Content</div>
+};
+
+export default EffectHook;
