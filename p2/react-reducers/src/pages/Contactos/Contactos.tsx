272eee0e3d28650092dcde33a387bbdcf4d1359e
Esau Martinez <esaumtz4722@gmail.com>
2022-10-27 20:01:22 -0500

    feat: add button to show dynamically the form

--- p2/react-reducers/src/pages/Contactos/Contactos.tsx
+++ p2/react-reducers/src/pages/Contactos/Contactos.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import ContactosReducer from "../../reducers/ContactosReducer";
 import { getLocalStorage } from "../../utilities/localstorage.utility";
 import { Formulario, TableContactos } from "./components";
@@ -10,13 +10,20 @@ const initialState = () => JSON.parse(getLocalStorage("contactos") as any) || []
 const Contactos = ({ }: ContactosInterface) => {
     const [state, dispatch] = useReducer(ContactosReducer, [], initialState);
 
+    // state to handle the form to appear or not
+    const [showForm, setShowForm] = useState(false);
+
     useEffect(() => {
         localStorage.setItem("contactos", JSON.stringify(state));
     }, [state]);
 
     return (
         <div className="container mt-3">
-            <Formulario dispatch={dispatch} />
+            <button className="btn btn-success" onClick={() => setShowForm(!showForm)}>
+                {showForm ? "Ocultar Formulario" : "Agregar Contacto"}
+            </button>
+            {showForm && <Formulario dispatch={dispatch} />}
+
             <TableContactos contactos={state} dispatch={dispatch} />
         </div>
     )
