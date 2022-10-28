86676bcab07e0aeeb5b2adb0cfabc2ea0d0a57c6
Esau Martinez <esaumtz4722@gmail.com>
2022-10-27 19:52:26 -0500

    feat: add localstorage strategy to handle data

--- p2/react-reducers/src/pages/Contactos/Contactos.tsx
+++ p2/react-reducers/src/pages/Contactos/Contactos.tsx
@@ -1,35 +1,23 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import ContactosReducer from "../../reducers/ContactosReducer";
+import { getLocalStorage } from "../../utilities/localstorage.utility";
 import { Formulario, TableContactos } from "./components";
 
 export interface ContactosInterface { };
 
-const contactos = [
-    {
-        id: "1",
-        nombre: "Juan",
-        numero: "123456789"
-    },
-    {
-        id: "2",
-        nombre: "Pedro",
-        numero: "987654321"
-    },
-    {
-        id: "3",
-        nombre: "Maria",
-        numero: "123456789"
-    }
-];
+const initialState = () => JSON.parse(getLocalStorage("contactos") as any) || [];
 
 const Contactos = ({ }: ContactosInterface) => {
-    const [state, dispatch] = useReducer(ContactosReducer, contactos);
-    console.log(state);
+    const [state, dispatch] = useReducer(ContactosReducer, [], initialState);
+
+    useEffect(() => {
+        localStorage.setItem("contactos", JSON.stringify(state));
+    }, [state]);
 
     return (
         <div className="container mt-3">
             <Formulario dispatch={dispatch} />
-            <TableContactos contactos={state} />
+            <TableContactos contactos={state} dispatch={dispatch} />
         </div>
     )
 };
