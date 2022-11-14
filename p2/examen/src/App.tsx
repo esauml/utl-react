8390b27435e340c94ede469943e6cae1a57dc8a6
Esau Martinez <esaumtz4722@gmail.com>
2022-11-13 22:08:56 -0600

    feat: add navbar

--- p2/examen/src/App.tsx
+++ p2/examen/src/App.tsx
@@ -1,9 +1,11 @@
 import { Participantes } from "./pages";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
 
   return (
     <>
+      <Navbar />
       <h1>My App</h1>
       <Participantes />
     </>
--- p2/examen/src/components/index.ts
+++ p2/examen/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Form';
 export * from './List';
+export * from './Navbar';
